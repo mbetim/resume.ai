b6f7b2fe41c270afe88214ccaf24b1116e1eb2f6
Matheus Betim <matheus.betim@synvia.com>
2024-11-08 14:18:32 -0300

    feat: add a function to send an whatsapp message

---
+++ src/whatsapp.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { env } from "./env";
+
+const api = axios.create({
+  baseURL: "https://graph.facebook.com/v21.0",
+  headers: {
+    Authorization: `Bearer ${env.WHATSAPP_TOKEN}`,
+  },
+});
+
+export const whatsapp = {
+  sendMessage: async (to: string, content: string) => {
+    const { data } = await api.post<unknown>("/491199940739841/messages", {
+      messaging_product: "whatsapp",
+      recipient_type: "individual",
+      to,
+      type: "text",
+      text: {
+        body: content,
+      },
+    });
+
+    return data;
+  },
+};
